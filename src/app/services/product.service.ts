d7f4d9ca8f8bdf98978c50b2997d9a5b4043906e
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-05-06 13:55:17 +0200

    CRUD method definition in service

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,24 @@
+import { Product } from './../interfaces/product';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) {  }
+
+  getProducts() {}
+
+  getProduct(id: string) {}
+
+  createProduct(product: Product) {}
+
+  deleteProduct(id: string) {}
+
+  updateProduct(id: string, product: Product) {}
+
 }
