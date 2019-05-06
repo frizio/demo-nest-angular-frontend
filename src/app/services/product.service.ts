0eca63b393bbec3b5fd0fecf0ca42425f910187a
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-05-06 14:05:28 +0200

    Add return type in CRUD method definitions

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,24 +1,37 @@
 import { Product } from './../interfaces/product';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
+  BASE_URL = 'http://localhost:3000';
+
   constructor(
     private http: HttpClient
   ) {  }
 
-  getProducts() {}
+  getProducts(): Observable<Product[]> {
+    return;
+  }
 
-  getProduct(id: string) {}
+  getProduct(id: string): Observable<Product> {
+    return;
+  }
 
-  createProduct(product: Product) {}
+  createProduct(product: Product): Observable<Product> {
+    return;
+  }
 
-  deleteProduct(id: string) {}
+  deleteProduct(id: string): Observable<Product> {
+    return;
+  }
 
-  updateProduct(id: string, product: Product) {}
+  updateProduct(id: string, product: Product): Observable<Product> {
+    return;
+  }
 
 }
