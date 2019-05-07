feada73470e3071920e4f154252f8497c34ca351
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-05-07 12:49:05 +0200

    Display list of products in json format

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,12 +1,14 @@
+import { Product } from './../../interfaces/product';
 import { ProductService } from './../../services/product.service';
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
+
+  public products: Product[] = [];
 
   constructor(
     private productService: ProductService
@@ -19,7 +21,8 @@ export class ProductListComponent implements OnInit {
   getProducts() {
     this.productService.getProducts().subscribe(
       res => {
-        console.log(res);
+        console.log('OK');
+        this.products = res;
       },
       err => {
         console.log(err);
